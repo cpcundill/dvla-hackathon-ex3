93eba246fccd314bb0cbbf419292d46dbccc51eb
Chris Cundill <c.cundill@kainos.com>
2014-07-30 08:51:01 +0100

    Added support for cross origin requests for Javascript

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -4,7 +4,11 @@ import config.DriverSearchConfig;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import resource.SearchDriverLocationResource;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
 
 public class App extends Application<DriverSearchConfig> {
 
@@ -19,6 +23,9 @@ public class App extends Application<DriverSearchConfig> {
     @Override
     public void run(DriverSearchConfig config, Environment environment) throws Exception {
         environment.jersey().register(new SearchDriverLocationResource());
+        //for cross-site resource calls
+        environment.servlets().addFilter("cors-filter", CrossOriginFilter.class)
+                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
     }
 
     public static void main(String[] args) throws Exception {
